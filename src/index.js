87439fcdbd1893dfaab4f33b6512575d116472cd
pduren <57735971+kParkz@users.noreply.github.com>
2020-05-20 10:20:03 -0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
